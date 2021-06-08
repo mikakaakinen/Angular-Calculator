f6affc500c71a07c0f309852dae1073e30935ab8
Mika Kaakinen <mika.kaakinen@gmail.com>
2021-06-08 15:18:15 +0300

    My Netlify Angular Calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
